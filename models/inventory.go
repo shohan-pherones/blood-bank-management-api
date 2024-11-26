79b8d9d017a23b8f74c1e600837a774289f3bf1c
shohan-pherones <shohan.sub.56@gmail.com>
2024-11-26 06:26:16 +0600

    create inventory model

--- models/inventory.go
+++ models/inventory.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Inventory struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	BloodType  BloodType          `bson:"blood_type" json:"blood_type"`
+	Quantity   int                `bson:"quantity" json:"quantity"`
+	ExpiryDate string             `bson:"expiry_date" json:"expiry_date"`
+	ReceivedAt primitive.DateTime `bson:"received_at" json:"received_at"`
+	UpdatedAt  primitive.DateTime `bson:"updated_at" json:"updated_at"`
+}
